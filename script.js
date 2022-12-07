d28fb4c743d15a059fa39972c9cc960c336140b9
Mayadev3 <107764065+Mayadev3@users.noreply.github.com>
2022-12-07 21:29:03 +0100

    created quiz questions and answers in an array of objects

---
+++ script.js
@@ -0,0 +1,34 @@
+const quizData = [
+  {
+    question: "Which language runs in a web browser?",
+    a: "Java",
+    b: "C++",
+    c: "Python",
+    d: "JavaScript",
+    correct: "d",
+  },
+  {
+    question: "What does CSS stand for?",
+    a: "Central Style Sheets",
+    b: "Cascading Style Sheets",
+    c: "Cascading Simple sheets",
+    d: "Cars, SUVs, Sailboats",
+    correction: "b",
+  },
+  {
+    question: "What does HTML stand for?",
+    a: "Hypertext Markup Language",
+    b: "Hypertext Markdown Language",
+    c: "Hyperloop Machine Learning",
+    d: "Helicopters Terminals Motors Logistics",
+    correction: "a",
+  },
+  {
+    question: "What year was JavaScript launched?",
+    a: "1996",
+    b: "1995",
+    c: "1994",
+    d: "none of the above",
+    correction: "b",
+  },
+];
